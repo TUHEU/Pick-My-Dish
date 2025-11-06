4b08f8681302fb41b3fe46b9dce11fa409bc1eeb
Fahdil <nadaljunior999@gmail.com>
2025-11-06 16:50:39 +0100

    Implement routing for splash and home screens

--- lib/route.dart
+++ lib/route.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import '../screens/splash_screen.dart';
+import '../screens/home_screen.dart';
 
+class AppRoutes {
+  static const String splash = '/';
+  static const String home = '/home';
+
+  static Map<String, WidgetBuilder> get routes {
+    return {
+      splash: (context) => const SplashScreen(),
+      home: (context) => const HomeScreen(),
+    };
+  }
+
+  static void navigateTo(BuildContext context, String routeName) {
+    Navigator.pushNamed(context, routeName);
+  }
+
+  static void replaceWith(BuildContext context, String routeName) {
+    Navigator.pushReplacementNamed(context, routeName);
+  }
+}
